87259388b3cc63cd9f30c2a389b539245b1464ac
Viswakarma neethu <136426858+Viswakarma-neethu@users.noreply.github.com>
2025-08-28 14:23:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myName = "Viswakarma Neethu";
 const today = new Date();
 console.log(`Hello, I am ${myName}. Today's date is ${today}`);
-console.log("win from feature-helin branch");
+console.log("this is feature-hello");
+
 
 
